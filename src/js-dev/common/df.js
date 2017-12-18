5636e798194e19a01b506525c98829913ef44c5b
18320867985 <huangqishanyx@163.com>
2017-12-18 11:42:28 +0800

    2107-12-18 am

---
+++ src/js-dev/common/df.js
@@ -0,0 +1,33 @@
+mui.init({
+	swipeBack: true //启用右滑关闭功能
+});
+
+
+//页脚链接跳转
+$(".mui-bar.mui-bar-tab").on("tap",".mui-tab-item", function() {
+
+	window.location.href = $(this).attr("href");
+
+});
+
+//兼容mobile
+$("head meta").each(function() {
+
+	var metaVal = $(this).attr("name");
+	if(metaVal === "viewport") {
+		$(this).attr("content", "width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no");
+	}
+});
+
+// ajax 页脚模板
+var footerUrl = $(".index-footer").attr("data-url") || "footer.html";
+$.get(footerUrl, function(html) {
+
+	$(".index-footer").html(html);
+	$(".index-footer").find(".mui-tab-item").removeClass("mui-active");
+
+	var id = $(".index-footer").attr("data-active") || 1;
+
+	$(".index-footer").find(".mui-tab-item").eq(id - 1).addClass("mui-active");
+
+});
